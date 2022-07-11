cbb21374fa5e0c2a148f95fd569baa0642b5b6c2
Tester <tester@testing>
2022-07-11 13:59:51 -0400

    fix user url

--- tests/hasql-odyssey.sh
+++ tests/hasql-odyssey.sh
@@ -15,12 +15,12 @@ for th in true false; do
                 echo
                 echo "Processing $PROFILE_TAG"
                 # Run sidecars
-                ./odyssey/build/sources/odyssey ./odyssey.conf > logs/hasql-odyssey-pooler-$PROFILE_TAG.log 2> logs/hasql-odyssey-pooler-$PROFILE_TAG.err & echo $! >> var/pid
+                ./odyssey/build/sources/odyssey ./odyssey.conf > logs/hasql-odyssey-pooler-$PROFILE_TAG.log 2> logs/hasql-odyssey-pooler-$PROFILE_TAG.err & write_pid
                 sleep 1
-                POSTGRES="host=localhost port=6432 user=$USER password=$PGPASSWORD dbname=$USER" ./hasql-loadtest-template/bin/testing-service -p 9000 > logs/hasql-odyssey-server-9000-$PROFILE_TAG.log 2> logs/hasql-odyssey-server-9000-$PROFILE_TAG.err & echo $! >> var/pid
-                POSTGRES="host=localhost port=6432 user=$USER password=$PGPASSWORD dbname=$USER" ./hasql-loadtest-template/bin/testing-service -p 9001 > logs/hasql-odyssey-server-9001-$PROFILE_TAG.log 2> logs/hasql-odyssey-server-9001-$PROFILE_TAG.err & echo $! >> var/pid
+                POSTGRES="host=localhost port=6432 user=$USER password=$PGPASSWORD dbname=$USER" ./hasql-loadtest-template/bin/testing-service -p 9000 > logs/hasql-odyssey-server-9000-$PROFILE_TAG.log 2> logs/hasql-odyssey-server-9000-$PROFILE_TAG.err & write_pid
+                POSTGRES="host=localhost port=6432 user=$USER password=$PGPASSWORD dbname=$USER" ./hasql-loadtest-template/bin/testing-service -p 9001 > logs/hasql-odyssey-server-9001-$PROFILE_TAG.log 2> logs/hasql-odyssey-server-9001-$PROFILE_TAG.err & write_pid
                 sleep 2
-                (while true; do curl -s "http://localhost:9001/$PROFILE_URL"; sleep 1; done) > /dev/null & echo $! >> var/pid
+                (while true; do curl -s "http://localhost:9001/hasql/flag?$PROFILE_URL"; sleep 1; done) > /dev/null & write_pid
                 # Run main tester
                 echo Running $PROFILE_TAG
                 ./wrk2/wrk -d 60 -t 2 -c 2000 --rate 2000 "http://localhost:9000/hasql/flag?$PROFILE_URL" | tee logs/hasql-odyssey-wrk2-${PROFILE_TAG}.log
