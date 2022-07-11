e7fb6c1985f72c22ff2067e8f5d51e1e59e43b88
Vitalii Guzeev <viviag@yandex.ru>
2022-07-11 16:08:24 +0300

    Update compute_run.sh (colors add excess columns)

--- compute_run.sh
+++ compute_run.sh
@@ -2,6 +2,6 @@
 
 cat $1 \
   | grep "Status" \
-  | awk '{print $4}' \
+  | awk '{print $NF}' \
   | LC_NUMERIC="C" awk '{print substr($1,length($1),1)=="m"?substr($1, 1, length($1)-1)*60:substr($1, 1, length($1)-1)}' \
   | ministat
